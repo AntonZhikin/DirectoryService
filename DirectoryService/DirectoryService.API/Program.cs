10de7b8b90ef25173247b8c366238b68c1624249
AntonZhikin <antonzhikin0@gmail.com>
2025-12-25 18:36:18 +0300

    add middleware and seq

--- DirectoryService/DirectoryService.API/Program.cs
+++ DirectoryService/DirectoryService.API/Program.cs
@@ -1,10 +1,18 @@
+using DirectoryService.API.Middlewares;
 using DirectoryService.Application;
 using DirectoryService.Application.Locations;
 using DirectoryService.Application.Validation;
 using DirectoryService.Infrastructure;
 using FluentValidation;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") ?? throw new Exception("Not found connection string Seq"))
+    .CreateLogger();
 
 builder.Services.AddControllers();
 
@@ -18,13 +26,19 @@ builder.Services.AddScoped<CreateLocationHandler>();
 
 builder.Services.AddValidatorsFromAssembly(typeof(CustomValidators).Assembly);
 
+builder.Services.AddSerilog();
+
 var app = builder.Build();
+
+app.UseExceptionMiddleware();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
 }
+
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 
