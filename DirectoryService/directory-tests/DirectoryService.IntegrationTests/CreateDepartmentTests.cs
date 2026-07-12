3a4de8ad38797ff13a9ddb67f53682e0637db30a
anton <antonzhikin0@gmail.com>
2026-07-12 18:43:42 +0300

    dsd

---
+++ DirectoryService/directory-tests/DirectoryService.IntegrationTests/CreateDepartmentTests.cs
@@ -0,0 +1,57 @@
+﻿using DirectoryService.Application.Departments.Commands.Create;
+using DirectoryService.Contracts.Request.Department;
+using DirectoryService.Domain.Locations;
+using DirectoryService.Domain.Locations.ValueObjects;
+using DirectoryService.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using TimeZone = DirectoryService.Domain.Locations.ValueObjects.TimeZone;
+
+namespace DirectoryService.IntegrationTests;
+
+public class CreateDepartmentTests : DirectoryTestWebFactory
+{
+    [Fact]
+    public async Task CreateDepartment_With_Valid_Data_Should_Succeed()
+    {
+        // arrange   
+        LocationId locationId = await CreateLocation();
+
+        await using var scope = Services.CreateAsyncScope(); 
+        
+        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+        
+        var cancellationToken = CancellationToken.None;
+
+        var command = new CreateDepartmentCommand(new CreateDepartmentRequest(
+
+            "Подразделение",
+            "sales",
+            null,
+            [locationId.Value]));
+
+        // act
+        var result = await mediator.Send(command, cancellationToken);
+        
+        // assert
+        Assert.True(result.IsSuccess);
+        Assert.NotEqual(Guid.Empty, result.Value.Value);
+    }
+
+    private async Task<LocationId> CreateLocation()
+    {
+        await using var initializerScope = Services.CreateAsyncScope();
+        var dbContext = initializerScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            
+        var location = new Location(
+            LocationName.Create("Локация").Value,
+            TimeZone.Create("Europe/Moscow").Value,
+            new Address("москва","пушкина", "12", "10"));
+
+        dbContext.Add(location);
+            
+        await dbContext.SaveChangesAsync();
+
+        return location.Id;
+    }
+}
---
+++ DirectoryService/directory-tests/DirectoryService.IntegrationTests/DirectoryTestWebFactory.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DirectoryService.IntegrationTests;
+
+public class DirectoryTestWebFactory : WebApplicationFactory<Program>;
